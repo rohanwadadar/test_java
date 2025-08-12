bf0270b8dffb16a77f6035616667bd5698358673
Rohan Wadadar <135444253+rohanwadadar@users.noreply.github.com>
2025-08-12 14:19:42 +0530

    Update formulaTest.java

--- formulaTest.java
+++ formulaTest.java
@@ -1,12 +1,13 @@
-package com.rohan.college.college;
-import org.junit.jupiter.api.TestInstance;
+package college;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.hibernate.sql.ast.tree.expression.Format;
 import org.junit.jupiter.api.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class formulaTest {
 
-    formula formula;
+    college.formula formula;
 
     @BeforeAll
     void beforeAllTests() {
@@ -19,20 +20,25 @@ class formulaTest {
         System.out.println("After All Tests");
     }
 
-    
     @Test
+    @Order (2)
    protected  void testAdd() {
-        int result = formula.add(3, 7);
-        assertEquals(10, result);
+       // int result = formula.add(3, 7);
+        assertEquals(10, formula.add(3, 7));
+        assertEquals(10, formula.add(5, 5));
+        assertEquals(10, formula.add(8, 2));
+        
     }
 
     @Test
+    @Order (1)
     public void testAreaOfCircle() {
         double result = formula.areaOfCircle(1);
         assertEquals(Math.PI, result, 0.0001);
     }
     
-    
+    @Order (3)
+    @RepeatedTest (3)
     @Test
     public void testpass() {
         double result = formula.password(190);
